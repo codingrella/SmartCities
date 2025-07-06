2fb129c005f1dcbccdd4d12c10e3bb049c2d9a51
VSBeck <92862665+VSBeck@users.noreply.github.com>
2025-07-06 14:05:59 +0200

    Update main.py

--- Hardware/main.py
+++ Hardware/main.py
@@ -5,3 +5,18 @@ Created on Sun Jul  6 11:44:35 2025
 @author: Vivienne Beck
 """
 
+import grovepi
+
+
+sound_sensor = 5
+
+
+grovepi.pinMode(sound_sensor, "OUTPUT")
+
+while True:
+    sound_level = grovepi.digitalRead(sound_sensor)
+    print(sound_level)
+    print('-----')
+    sound_level = grovepi.analogRead(sound_sensor)
+    print(sound_level)
+    print('=====')
