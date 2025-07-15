465ff9f141e5e7f897c878080dfdcfb16c61772e
VSBeck <92862665+VSBeck@users.noreply.github.com>
2025-07-15 15:02:11 +0200

    Update MQTTInterface.py

--- Hardware/MQTTInterface.py
+++ Hardware/MQTTInterface.py
@@ -7,14 +7,13 @@ import time
 from paho.mqtt import client as mqtt_client
 
 
-FIRST_RECONNECT_DELAY = 1
-RECONNECT_RATE = 2
-MAX_RECONNECT_COUNT = 12
-MAX_RECONNECT_DELAY = 60
+
 
 
 class __MQTTPublisher:
     def __init__(self, room, deviceType, topic):
+        self.deviceType = deviceType
+        self.deviceName = topic
         self.broker = '192.168.188.40'
         self.port = 1883
         self.topic = f"library/{room}/{deviceType}/{topic}"
@@ -32,58 +31,27 @@ class __MQTTPublisher:
         client.connect(self.broker, self.port)
         return client
     
-    
-    def publish(self, msg):
-        result = self.client.publish(self.topic, msg)
-        # result: [0, 1]
-        status = result[0]
-        if status == 0:
-            print(f"Send `{msg}` to topic `{self.topic}`")
-        else:
-            print(f"Failed to send message to topic {self.topic}")
             
     def disconnect(self):
         self.client.disconnect()
+        
+        
+    def publish(self, value):
+        t = time.localtime()
+        current_time = time.strftime("%H:%M:%S", t)
+
+        msg = str({f'{self.deviceType}': self.deviceName, 'Value': value, 'TimeStamp': current_time})
+        result = self.client.publish(self.topic, msg)
     
     
     def run(self, value):
         self.client.loop_start()
         self.publish(value)
         self.client.loop_stop()
-
-
-class MQTTPublisher_Sensor(__MQTTPublisher):
-    def __init__(self, room, topic):
-        self.room = room
-        self.sensor = topic
         
-        super().__init__(self.room, 'Sensor', self.sensor)
-    
-    def publish(self, value):
-        t = time.localtime()
-        current_time = time.strftime("%H:%M:%S", t)
-
-        msg = str({'Sensor': self.sensor, 'Value': value, 'TimeStamp': current_time})
-        result = self.client.publish(self.topic, msg)
 
 
-class MQTTPublisher_Actuator(__MQTTPublisher):
-    def __init__(self, room, topic):
-        self.room = room
-        self.actuator = topic
-        
-        super().__init__(self.room, 'Actuator', self.actuator)
-    
-    def publish(self, value):
-        t = time.localtime()
-        current_time = time.strftime("%H:%M:%S", t)
-
-        msg = str({'Sensor': self.sensor, 'Value': value, 'TimeStamp': current_time})
-        result = self.client.publish(self.topic, msg)
-
-
-
-class __MQTTSubscriber:
+class MQTTSubscriber:
     def __init__(self, room, deviceType, topic):
         self.broker = '192.168.188.40'
         self.port = 1883
@@ -104,39 +72,15 @@ class __MQTTSubscriber:
         return client
     
     
+    def subscribe(self):
+        def on_message(client, userdata, msg):
+            print(msg.payload.decode())
+    
+        self.client.subscribe(self.topic)
+        self.client.on_message = on_message
+    
+    
     def run(self):
         self.subscribe()
         self.client.loop_forever()
-        
-      
-        
-class MQTTSubscriber_Sensor(__MQTTSubscriber):
-    def __init__(self, room, topic):
-        self.room = room
-        self.sensor = topic
-        
-        super().__init__(self.room, 'Sensor', self.sensor)
-    
-    def subscribe(self):
-        def on_message(client, userdata, msg):
-            print(msg.payload.decode())
-    
-        self.client.subscribe(self.topic)
-        self.client.on_message = on_message
-        
-        
-        
-class MQTTSubscriber_Actuator(__MQTTSubscriber):
-    def __init__(self, room, topic):
-        self.room = room
-        self.actuator = topic
-        
-        super().__init__(self.room, 'Actuator', self.actuator)
-    
-    def subscribe(self):
-        def on_message(client, userdata, msg):
-            print(msg.payload.decode())
-    
-        self.client.subscribe(self.topic)
-        self.client.on_message = on_message
         
