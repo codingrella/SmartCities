21e874ed9bca9c7b2278bb2cae61bd5d855e1a8e
VSBeck <92862665+VSBeck@users.noreply.github.com>
2025-07-17 20:44:50 +0200

    Add files via upload

---
+++ Hardware/valueThresholds.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+
+"""
+From GrovePi:
+    - Temperature Range: 0°C to 50°C
+    - Humidity Range: 20% to 80% RH
+    
+    
+"""
+
+inside_isLight = 600
+
+outside_isDark = 0
+outside_isVerySunny = 2
+
+temp_lower = 21
+temp_upper = 26
+
+hum_lower = 50
+hum_upper = 60 
