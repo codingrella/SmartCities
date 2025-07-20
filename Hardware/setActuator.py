6107f43f8b826b5c5b1316134d24a406d030f7b5
VSBeck <92862665+VSBeck@users.noreply.github.com>
2025-07-20 12:16:06 +0200

    Add files via upload

---
+++ Hardware/setActuator.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+
+
+import time
+import grovepi
+import threading
+
+from MQTTInterface import MQTTSubscriber, MQTTPublisher
+
+
+class setter(threading.Thread):
+    def __init__(self, room):
+        self.actuatorToFunc = { 'AC': self._setAC,
+                                'Heater': self._setHeater,
+                                'Blinds': self._setBlinds,
+                                'Light': self._setLights }
+        
+        self.actuatorPins = { 'AC': 3,
+                              'Heater': 6,
+                              'Blinds_up': 8,
+                              'Blinds_down': 5 }
+        
+        self._setPinModes()
+        
+        self.room = room
+        self.pub = MQTTPublisher()
+        
+        sub = MQTTSubscriber(self.room, 'Actuator', '#')
+        sub.client.on_message = self.on_message
+        
+        self.sub = threading.Thread(target=sub.run)
+        self.sub.start()
+        
+        
+    def _setPinModes(self):
+        for key, value in self.actuatorPins.items():
+            grovepi.pinMode(value, "INPUT")
+            
+    def _setAC(self, value):
+        grovepi.digitalWrite(self.actuatorPins['AC'], value)
+        
+    def _setHeater(self, value):
+        grovepi.digitalWrite(self.actuatorPins['Heater'], value)
+        
+    def _setBlinds(self, value):
+        if value == 1:
+            grovepi.digitalWrite(self.actuatorPins['Blinds_down'], 1)
+        elif value == 0:
+            grovepi.digitalWrite(self.actuatorPins['Blinds_up'], 1)
+            grovepi.digitalWrite(self.actuatorPins['Blinds_down'], 1)
+            
+        time.sleep(4)
+        grovepi.digitalWrite(self.actuatorPins['Blinds_up'], 0)
+        grovepi.digitalWrite(self.actuatorPins['Blinds_down'], 0)
+        
+    def _setLights(self, value):
+        if value == 1:
+            value = 'on'
+        elif value == 0:
+            value = 'off'
+            
+        self.pub.run('SR_1', 'Actuator', 'Light', value)
+        
+    def on_message(self, client, userdata, msg):
+        res = eval(msg.payload.decode())
+        self.actuatorToFunc[res['Device']](res['Value'])
+        
+            
