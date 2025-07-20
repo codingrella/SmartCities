52c856e9cc4bffc48662f7cbd056bfdfa90df398
VSBeck <92862665+VSBeck@users.noreply.github.com>
2025-07-21 01:11:04 +0200

    Update main.py

--- Hardware/main.py
+++ Hardware/main.py
@@ -26,7 +26,7 @@ digitalPorts = {
 
 actuatorPins = { 'Heater': 6,
                  'Blinds_up': 8,
-                 'Blinds_down': 5 }
+                 'Blinds_down': 3 }
 
 
 
@@ -38,24 +38,18 @@ def _setBlinds(value):
         grovepi.digitalWrite(actuatorPins['Blinds_down'], 1)
     elif value == 0:
         grovepi.digitalWrite(actuatorPins['Blinds_up'], 1)
-        grovepi.digitalWrite(actuatorPins['Blinds_down'], 1)
         
-    time.sleep(4)
+    time.sleep(6)
     grovepi.digitalWrite(actuatorPins['Blinds_up'], 0)
     grovepi.digitalWrite(actuatorPins['Blinds_down'], 0)
 
 
 
-
-
 if __name__ == "__main__":
-    # dbInterface_num = NumericDatabaseInterface()
     lightSensor = AnalogSensorInterface(analogPorts['LightSensor'])
     soundSensor = AnalogSensorInterface(analogPorts['SoundSensor'])
     
     PIR = DigitalSensorInterface(digitalPorts['PIR'])
-    # button = DigitalSensorInterface(digitalPorts['Button'])
-    
     
     pub = MQTTPublisher()
     getActuatorValues = getter('SR_1')
@@ -70,21 +64,12 @@ if __name__ == "__main__":
         
         movement = PIR.getData()
         pub.run('SR_1', 'Sensor', 'Motion_Sensor', movement)
-        # dbInterface_num.updateMotion(movement)
-        # print("MOVEMENT:\t" + str(movement))
-        
-        # sitting = button.getData()
-        # print("SITTING:\t" + str(sitting))
         
         lightLevel = lightSensor.getData()
         pub.run('SR_1', 'Sensor', 'Light_Sensor', lightLevel)
-        # dbInterface_num.updateLightLevel(lightLevel)
-        # print("LIGHT LEVEL:\t" + str(lightLevel))
         
         noiseLevel = soundSensor.getData()
         pub.run('SR_1', 'Sensor', 'Sound_Sensor', noiseLevel)
-        # dbInterface_num.updateNoiseLevel(noiseLevel)
-        #print("NOISE LEVEL:\t" + str(noiseLevel))
         
         text = getNoiseLevelText(noiseLevel)
         writeDataToLCD(text)
@@ -94,12 +79,20 @@ if __name__ == "__main__":
         if math.isnan(temp) == False and math.isnan(humidity) == False:
             pub.run('SR_1', 'Sensor', 'Temperature_Sensor', "%.02f"%temp)
             pub.run('SR_1', 'Sensor', 'Humidity_Sensor', "%.02f"%humidity)
-            # dbInterface_num.updateTemperature(temp)
-            # dbInterface_num.updateHumidity(humidity)
-            # print("temp =  C humidity =%.02f%%"%(temp, humidity))
             
-        _setHeater(getActuatorValues.actuatorValues['Heater'])
-        _setBlinds(getActuatorValues.actuatorValues['Blinds'])
-        
+        if getActuatorValues.actuatorValues['Heater_off'] == 1:
+            _setHeater(0)
+            getActuatorValues.actuatorValues['Heater_off'] = 0
+        elif getActuatorValues.actuatorValues['Heater_on'] == 1:
+            _setHeater(1)
+            getActuatorValues.actuatorValues['Heater_on'] = 0
+                
+        if getActuatorValues.actuatorValues['Blinds_down'] == 1:
+            _setBlinds(1)
+            getActuatorValues.actuatorValues['Blinds_down'] = 0
+        elif getActuatorValues.actuatorValues['Blinds_up'] == 1:
+            _setBlinds(0)
+            getActuatorValues.actuatorValues['Blinds_up'] = 0
+            
         print('<3', flush=True)
         
