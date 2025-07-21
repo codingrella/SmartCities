01ff3c142716e0bfb1431ca8ba93dff0a5151553
VSBeck <92862665+VSBeck@users.noreply.github.com>
2025-07-21 06:05:28 +0200

    Update AIPlanner.py

--- Hardware/AIPlanner.py
+++ Hardware/AIPlanner.py
@@ -1,116 +1,386 @@
-( define (domain studyRoom)
+# -*- coding: utf-8 -*-
 
-(:types room system -object
-        airConditioning light blinds heater -system 
-        )
 
-( :predicates 
-    ; -------------------------------------- SENSORS --------------------------------------
-    ; TEMPERATURE SENSOR
-    (temp_isCold ?r -room)
-    (temp_isGood ?r -room)
-    (temp_isHot ?r -room)
-    ; HUMIDITY SENSOR 
-    (hum_isGood ?r -room)
-    (hum_isMid ?r -room)
-    (hum_isBad ?r -room)
-    ; LIGHT SENSOR
-    (outside_isVerySunny ?r -room)
-    (outside_isDark ?r -room)
-    (inside_isLight ?r -room)
-    ; MOTION SENSOR
-    (motion_detected ?r -room)
-    ; SEAT 'SENSOR'
-    (seat_occupied  ?r -room)
+
+
+import os
+import time
+import shutil
+import subprocess
+import threading
+
+from datetime import datetime
+from MQTTInterface import MQTTSubscriber
+from MQTTInterface import MQTTPublisher
+
+
+MAX_RANGE_MOTION_DETECTED = 1*60            # 5 minutes
+OPENING_HOUR = datetime.strptime('08:00:00', '%H:%M:%S')
+CLOSING_HOUR = datetime.strptime('20:00:00', '%H:%M:%S')
+
+DOMAIN_FILE_PATH = r"/PDDL_Files/Domain.pddl"
+PROBLEM_FILE_PATH = r"/PDDL_Files/ProblemFile_SR_1.pddl"
+PLAN_RESULT_PATH = r"/PDDL_Files/Plan.txt"
+
+
+class AIPlannerInterface:
+    def __init__(self, room):
+        self.sensorValues = { 'Humidity_Sensor': 0,
+                              'Motion_Sensor': 0,
+                              'Light_Sensor': 0,
+                              'Outside_Sensor': 0,
+                              'Sound_Sensor': 0,
+                              'Temperature_Sensor': 0 }
+        self.actuatorValues = { 'Blinds': 0,
+                                'Light': 1,
+                                'AC': 1,
+                                'Heater': 0 }
+        self.thresholdValues = { 'inside_isLight': 600,
+                                 'outside_isDark': 0,
+                                 'outside_isVerySunny': 2,
+                                 'temp_lower': 21,
+                                 'temp_upper': 26,
+                                 'hum_lower': 50,
+                                 'hum_upper': 60 }
+        self.actions = { 'TURNONAC': self._setAC,
+                         'TURNOFFAC': self._setAC,
+                         'TURNONHEATER': self._setHeater,
+                         'TURNOFFHEATER': self._setHeater,
+                         'TURNONLIGHT': self._setLight,
+                         'TURNOFFLIGHT': self._setLight,
+                         'OPENBLINDS': self._setBlinds,
+                         'CLOSEBLINDS': self._setBlinds }
+        
+        self.room = room
+        
+        self.replan = True
+        self.noReplannedSinceMaxRange = False
+        self.time_toggleZeroDetected = datetime.now().strftime("%H:%M:%S")
+        # self.time_toggleOneDetected = datetime.now().strftime("%H:%M:%S")
+        
+        
+        self.pub = MQTTPublisher()
+        
+        sub = MQTTSubscriber(room, '+', '+')
+        sub.client.on_message = self.on_message
+        
+        self.sub = threading.Thread(target=sub.run)
+        self.sub.start()
+        
     
-    ; SaveEnergy Goals, 
-    ; combines actuator states (for multiple actuators of same kind)
-    (saveEnergy_acs ?r -room)
-    (saveEnergy_lights ?r -room)
-    (saveEnergy_heater ?r -room)
-    (stopBrightness ?r -room)
+    def setInitialState(self):
+        self._setAC(self.actuatorValues['AC'])
+        self._setHeater(self.actuatorValues['Heater'])
+        self._setLight(self.actuatorValues['Light'])
+        self._setBlinds(self.actuatorValues['Blinds'])
+        
+        
     
-    ; -------------------------------------- SYSTEM --------------------------------------
-    ; AIRCONDITIONING
-    (airConditioning_on ?ac -airConditioning ?r -room)
-    ; LIGHTING
-    (lighting_on ?l -light ?r -room)
-    ; BLINDS 
-    (blinds_down ?b -blinds ?r -room)
-    ; heater
-    (heater_on ?h -heater ?r -room)
+    def on_message(self, client, userdata, msg):
+        # On manual intervention from UI
+        if 'light_on' == msg.payload.decode(): 
+            self.actuatorValues['Light'] = 1
+            return
+        elif 'light_off' == msg.payload.decode():
+            self.actuatorValues['Light'] = 0
+            return
+        elif 'ac_on' == msg.payload.decode(): 
+            self.actuatorValues['AC'] = 1
+            return
+        elif 'ac_off' == msg.payload.decode():
+            self.actuatorValues['AC'] = 0
+            return
+        elif 'up' == msg.payload.decode(): 
+            self.actions['OPENBLINDS'](0)
+            return
+        elif 'down' == msg.payload.decode():
+            self.actions['CLOSEBLINDS'](1)
+            return
+       
+        # On hardware updates
+        res = eval(msg.payload.decode())
+        
+        if res['Device'] == 'Motion_Sensor':
+            if res['Value'] == 1:
+                self.sensorValues[res['Device']] = 1
+                self.time_toggleZeroDetected = -1
+                self.replan = True
+            elif res['Value'] == 0 and self.sensorValues[res['Device']] == 1:
+                self.noReplannedSinceMaxRange = True
+                self.time_toggleZeroDetected = res['TimeStamp']
+        
+        if 'Outside_Sensor' == res['Device']:
+                self.replan = True
+                
+                
+        if 'Sensor' in res['Device']:
+            self.sensorValues[res['Device']] = res['Value']
+        elif 'Threshold_Low' in res['Device'] and 'Temp' in res['Device']:
+            self.thresholdValues['temp_lower'] = float(res['Value'])
+            print('NEW THRESHOLD, TEMPERATURE LOW:   ' + str(res['Value']))
+            self.replan = True
+        elif 'Threshold_High' in res['Device'] and 'Temp' in res['Device']:
+            self.thresholdValues['temp_upper'] = float(res['Value'])
+            print('NEW THRESHOLD, TEMPERATURE UPPER:   ' + str(res['Value']))
+            self.replan = True
+        elif 'Humidity_Threshold' != res['Device'] and 'Light' != res['Device']:
+            self.actuatorValues[res['Device']] = res['Value']
     
-)
-
-(:action turnOnAC
-    :parameters (?ac -airConditioning ?h -heater ?r -room)
-    :precondition (and (not (airConditioning_on ?ac ?r))
-                       (not (heater_on ?h ?r))
-                       (or (temp_isHot ?r)
-                       (hum_isBad ?r)))
-    :effect (and (temp_isGood ?r)
-                 (hum_isGood ?r)
-                 (airConditioning_on ?ac ?r))
-)
-
-(:action turnOffAC
-    :parameters (?ac -airConditioning ?r -room)
-    :precondition (and (airConditioning_on ?ac ?r))
-    :effect (and (saveEnergy_acs ?r)
-                 (not (airConditioning_on ?ac ?r)))
-)
-
-(:action turnOnHeater
-    :parameters (?h -heater ?ac -airConditioning ?r -room)
-    :precondition (and (not(heater_on ?h ?r))
-                       (not (airConditioning_on ?ac ?r))
-                       (temp_isCold ?r))
-    :effect (and (temp_isGood ?r)
-                 (heater_on ?h ?r))
-)
-
-(:action turnOffHeater
-    :parameters (?h -heater ?r -room)
-    :precondition (and (heater_on ?h ?r))
-    :effect (and (saveEnergy_heater ?r)
-                 (not (heater_on ?h ?r)))
-)
-
-(:action turnOnLight
-    :parameters (?l -light ?r -room)
-    :precondition (and (not (inside_isLight ?r))
-                       (motion_detected ?r)
-                       (not(lighting_on ?l ?r)))
-    :effect (and (inside_isLight ?r)
-                 (lighting_on ?l ?r))
-)
-
-(:action turnOffLight
-    :parameters (?l -light ?r -room)
-    :precondition (and (not (outside_isVerySunny ?r))
-                       (not(motion_detected ?r))
-                       (lighting_on ?l ?r))
-    :effect (and (saveEnergy_lights ?r)
-                 (not(lighting_on ?l ?r)))
-)
-
-; also close blinds if light is turned on?
-(:action closeBlinds
-    :parameters (?b -blinds ?r -room)
-    :precondition (and  (outside_isVerySunny ?r)
-                        (not(blinds_down ?b ?r)))
-    :effect (and (not (inside_isLight ?r))
-                 (blinds_down ?b ?r)
-                 (stopBrightness ?r))
-)
-
-(:action openBlinds
-    :parameters (?b -blinds ?r -room)
-    :precondition (and  (not (outside_isVerySunny ?r))
-                        (not (outside_isDark ?r))
-                        (blinds_down ?b ?r))
-    :effect (and (inside_isLight ?r)
-                 (not(blinds_down ?b ?r)))
-)
-
-)
+    
+    def createAIProblemFile(self, inits, goals):
+        dirname = os.path.dirname(__file__)
+        
+        src = dirname + f'/PDDL_Files/ProblemFile_{self.room}_Temp.pddl'
+        dst = dirname + f'/PDDL_Files/ProblemFile_{self.room}.pddl'
+        
+        path = shutil.copy2(src,dst)
+        
+        text_init = "(:init\n"
+        for init in inits:
+            text_init += ('\t' + init + '\n')
+        text_init += ')\n\n'
+        
+        text_goal = "(:goal (and\n"
+        for goal in goals:
+            text_goal += ('\t' + goal + '\n')
+        text_goal += ') )\n)'
+        
+        with open(path, "a") as myfile:
+            myfile.write(text_init)
+            myfile.write(text_goal)
+            
+    
+    def _setAC(self, value):
+        if value == 'ac_on':
+            value = 1
+        elif value == 'ac_off':
+            value = 0
+            
+        self.actuatorValues['AC'] = value
+        
+        if value == 1:
+            value = 'ac_on'
+        elif value == 0:
+            value = 'ac_off'
+            
+        self.pub.run(f'{self.room}', 'Actuator', 'AC', value)
+        
+    def _setHeater(self, value):
+        self.actuatorValues['Heater'] = value
+        self.pub.run(f'{self.room}', 'Actuator', 'Heater', value)
+    
+    def _setLight(self, value):
+        if value == 'light_on':
+            value = 1
+        elif value == 'light_off':
+            value = 0
+            
+        self.actuatorValues['Light'] = value
+        
+        if value == 1:
+            value = 'light_on'
+        elif value == 0:
+            value = 'light_off'
+        self.pub.run(f'{self.room}', 'Actuator', 'Light', value)
+    
+    def _setBlinds(self, value):
+        print(value)
+        print(self.actuatorValues['Blinds'])
+        if self.actuatorValues['Blinds'] != value:
+            self.actuatorValues['Blinds'] = value
+            self.pub.run(f'{self.room}', 'Actuator', 'Blinds', value)
+        
+       
+    def _getHumidityInit(self):
+        if float(self.sensorValues['Humidity_Sensor']) < self.thresholdValues['hum_lower']:
+            return f'(hum_isGood {self.room})'
+        elif float(self.sensorValues['Humidity_Sensor']) < self.thresholdValues['hum_upper']:
+            return f'(hum_isMid {self.room})'
+        else:
+            return f'(hum_isBad {self.room})'
+        
+    def _getTempInit(self):
+        if float(self.sensorValues['Temperature_Sensor']) < self.thresholdValues['temp_lower']:
+            return f'(temp_isCold {self.room})'
+        elif float(self.sensorValues['Temperature_Sensor']) < self.thresholdValues['temp_upper']:
+            return f'(temp_isGood {self.room})'
+        else:
+            return f'(temp_isHot {self.room})'
+        
+    def _getInsideLightInit(self):
+        if int(self.sensorValues['Light_Sensor']) > self.thresholdValues['inside_isLight']:
+            return f'(inside_isLight {self.room})'
+        else:
+            return ''
+        
+    def _getOutsideLightInit(self):
+        if int(self.sensorValues['Outside_Sensor']) == self.thresholdValues['outside_isDark']:
+            return f'(outside_isDark {self.room})'
+        elif int(self.sensorValues['Outside_Sensor']) == self.thresholdValues['outside_isVerySunny']:
+            return f'(outside_isVerySunny {self.room})'
+        else:
+            return ''
+            
+    def _getMotionInit(self):
+        if int(self.sensorValues['Motion_Sensor']) == 1:
+            return f'(motion_detected {self.room})'
+        else:
+            return ''
+        
+    def _getActuatorInit(self):
+        if self.room == 'SR_1': roomNr = '1'
+        elif self.room == 'SR_2': roomNr = '2'
+        
+        
+        inits = []
+        print('ACTUATOR INIT')
+        print(self.actuatorValues['Blinds'])
+        if int(self.actuatorValues['Blinds']) == 1:
+            inits.append(f'(blinds_down b{roomNr} {self.room})')
+        if not self.actuatorValues['Light'] == 'light_off' and (self.actuatorValues['Light'] == 'light_on' or int(self.actuatorValues['Light']) == 1):
+           inits.append(f'(lighting_on l{roomNr} {self.room})')
+        if not self.actuatorValues['AC'] == 'ac_off' and (self.actuatorValues['AC'] == 'ac_on' or int(self.actuatorValues['AC']) == 1):
+            inits.append(f'(airConditioning_on ac{roomNr} {self.room})')
+        if int(self.actuatorValues['Heater']) == 1:
+            inits.append(f'(heater_on h{roomNr} {self.room})')
+            
+        return inits
+        
+        
+    def getAIPlannerInits(self):
+        result = []
+        result.append(self._getHumidityInit())
+        result.append(self._getTempInit())
+        result.append(self._getInsideLightInit())
+        result.append(self._getOutsideLightInit())
+        result.append(self._getMotionInit())
+        
+        result.extend(self._getActuatorInit())
+        
+        return result
+    
+    def getAIPlannerGoals(self, inits):
+        goals = []
+        if self.room == 'SR_1': roomNr = '1'
+        elif self.room == 'SR_2': roomNr = '2'
+        
+        if f'(outside_isVerySunny {self.room})' in inits and f'(blinds_down b{roomNr} {self.room})' not in inits: 
+            goals.append(f'(stopBrightness {self.room})')
+            goals.append(f'(inside_isLight {self.room})')
+        elif f'(outside_isVerySunny {self.room})' not in inits and f'(outside_isDark {self.room})' not in inits: 
+            goals.append(f'(saveEnergy_lights {self.room})')
+            goals.append(f'(inside_isLight {self.room})')
+        elif f'(outside_isDark {self.room})' in inits: goals.append(f'(inside_isLight {self.room})')
+        
+        if f'(hum_isGood {self.room})' in inits and f'(temp_isGood {self.room})' in inits and f'(airConditioning_on ac{roomNr} {self.room})' in inits: goals.append(f'(saveEnergy_acs {self.room})')
+        elif f'(hum_isBad {self.room})' in inits: goals.append(f'(hum_isGood {self.room})')
+        elif f'(temp_isGood {self.room})' not in inits: goals.append(f'(temp_isGood {self.room})')
+        elif f'(temp_isGood {self.room})' in inits and f'(heater_on h{roomNr} {self.room})' in inits: goals.append(f'(saveEnergy_heater {self.room})')
+        
+        # if f'(motion_detected {self.room})' in inits: goals.append(f'(inside_isLight {self.room})')
+        # elif f'(motion_detected {self.room})' not in inits and f'(inside_isLight {self.room})' in inits: goals.append(f'(saveEnergy_lights {self.room})')
+        
+        
+        return goals
+            
+    def startPlanning(self):
+        inits = self.getAIPlannerInits()
+        print('INITS: ')
+        print(inits)
+        goals = self.getAIPlannerGoals(inits)
+        print('GOALS: ')
+        print(goals)
+        
+        self.createAIProblemFile(inits, goals)
+        
+        
+    def getPlanSteps(self, plannerResponse):
+        planSteps = []
+        
+        if 'ff: found legal plan as follows' in plannerResponse:
+            idx1 = plannerResponse.find('\nstep')
+            idx2 = plannerResponse.find('\n\ntime spent:', idx1 + len('step'))
+            
+            if idx1 != -1 and idx2 != -1:
+                combinedSteps = plannerResponse[idx1 + len('\nstep'):idx2]
+                combinedSteps = combinedSteps.split('\n')
+                
+                for step in combinedSteps:
+                    if ':' in step:
+                        action = step.split(':')[1]
+                        # remove white noise, but keep spaces between words
+                        action = " ".join(action.split())
+                        planSteps.append(action)
+        return planSteps
+                    
+    
+    def executePlan(self, steps):        
+        for step in steps:
+            elements = step.split(' ')
+            
+            if 'ON' in elements[0] or 'CLOSE' in elements[0]:
+                self.actions[elements[0]](1)
+            elif 'OFF' in elements[0] or 'OPEN' in elements[0]:
+                self.actions[elements[0]](0)
+                 
+        
+if __name__ == "__main__":
+    planner = AIPlannerInterface('SR_1')
+    planner.setInitialState()
+    
+    while True:
+        if planner.time_toggleZeroDetected is not -1:
+            time1 = datetime.strptime(planner.time_toggleZeroDetected, '%H:%M:%S')
+            time2 = datetime.strptime(datetime.now().strftime("%H:%M:%S"), '%H:%M:%S')
+            difference = time2 - time1
+            difference = int(difference.total_seconds())
+        else:
+            difference = 0
+        
+        # if OPENING_HOUR.time() >= datetime.now().time() or datetime.now().time() >= CLOSING_HOUR.time():
+        #     print('CLOSED')
+        #     planner.setInitialState()
+        
+        if (difference >= MAX_RANGE_MOTION_DETECTED) and planner.noReplannedSinceMaxRange: 
+            planner.sensorValues['Motion_Sensor'] = 0
+            print('MAX RANGE REACHED')
+            print('REPLANNING')
+            planner.startPlanning()
+            time.sleep(5)
+            planner.noReplannedSinceMaxRange = False
+            try: plannerResponse = subprocess.check_output(["./runPlan.sh"]).decode("utf-8")
+            except: pass
+            time.sleep(5)
+            planSteps = planner.getPlanSteps(plannerResponse)
+            print(planSteps)
+            planner.executePlan(planSteps)
+            
+        elif planner.replan:
+            print('REPLANNING')
+            planner.replan = False
+            planner.startPlanning()
+            time.sleep(5)
+            try: plannerResponse = subprocess.check_output(["./runPlan.sh"]).decode("utf-8")
+            except: pass
+            time.sleep(5)
+            planSteps = planner.getPlanSteps(plannerResponse)
+            print(planSteps)
+            planner.executePlan(planSteps)
+            
+        elif datetime.now().minute == 0 or datetime.now().minute == 30:
+            print('REPLANNING')
+            planner.startPlanning()
+            time.sleep(5)
+            try: plannerResponse = subprocess.check_output(["./runPlan.sh"]).decode("utf-8")
+            except: pass
+            time.sleep(5)
+            planSteps = planner.getPlanSteps(plannerResponse)
+            print(planSteps)
+            planner.executePlan(planSteps)
+    
+        
+    
+    # inits = AIPlanner.getAIPlannerInits('SR_1')    
+    # goals = AIPlanner.getAIPlannerGoals(inits, 'SR_1')
+    
+    # AIPlanner.createAIProblemFile(inits, goals, 'SR_1')
